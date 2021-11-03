b3894576813edc3458e78d369867c3688dc3e321
KojimaMcMaple <KyleHunter.KH@outlook.com>
2021-11-03 17:33:40 -0400

    receive Login & Create account request

--- SimplestServer/Assets/NetworkedServer.cs
+++ SimplestServer/Assets/NetworkedServer.cs
@@ -14,6 +14,8 @@ public class NetworkedServer : MonoBehaviour
     int hostID;
     int socketPort = 5491;
 
+    LinkedList<PlayerAccount> account_list_;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,7 +25,8 @@ public class NetworkedServer : MonoBehaviour
         unreliableChannelID = config.AddChannel(QosType.Unreliable);
         HostTopology topology = new HostTopology(config, maxConnections);
         hostID = NetworkTransport.AddHost(topology, socketPort, null);
-        
+
+        account_list_ = new LinkedList<PlayerAccount>();
     }
 
     // Update is called once per frame
@@ -66,5 +69,68 @@ public class NetworkedServer : MonoBehaviour
     private void ProcessReceivedMsg(string msg, int id)
     {
         Debug.Log("msg received = " + msg + ".  connection id = " + id);
+        string[] csv = msg.Split(',');
+        GlobalEnum.ClientToServerSignifier signifier = (GlobalEnum.ClientToServerSignifier)System.Enum.Parse(typeof(GlobalEnum.ClientToServerSignifier), csv[0]);
+
+        switch (signifier)
+        {
+            case GlobalEnum.ClientToServerSignifier.CreateAccount:
+                Debug.Log("GlobalEnum.ClientToServerSignifier.CreateAccount");
+                string n = csv[1];
+                string p = csv[2];
+                bool does_name_exist = false;
+                foreach (PlayerAccount item in account_list_)
+                {
+                    if (item.name == n)
+                    {
+                        does_name_exist = true;
+                        break;
+                    }
+                }
+                if (does_name_exist)
+                {
+                    SendMessageToClient(GlobalEnum.ServerToClientSignifier.AccountCreationFailed + "", id);
+                }
+                else
+                {
+                    PlayerAccount new_account = new PlayerAccount(n, p);
+                    account_list_.AddLast(new_account);
+                    SendMessageToClient(GlobalEnum.ServerToClientSignifier.AccountCreationComplete + "", id);
+                }
+                break;
+            case GlobalEnum.ClientToServerSignifier.Login:
+                Debug.Log("GlobalEnum.ClientToServerSignifier.Login");
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+public class PlayerAccount
+{
+    public string name, password;
+
+    public PlayerAccount(string name, string password)
+    {
+        this.name = name;
+        this.password = password;
+    }
+}
+
+public static class GlobalEnum //copied from NetworkedClient
+{
+    public enum ClientToServerSignifier
+    {
+        CreateAccount = 1,
+        Login
+    }
+
+    public enum ServerToClientSignifier
+    {
+        LoginComplete = 1,
+        LoginFailed,
+        AccountCreationComplete,
+        AccountCreationFailed
     }
 }
